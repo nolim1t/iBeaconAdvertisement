0c011df220c12701ece927ab0d22808a39d273ea
고수놀리밑™ <nolimit@perceptionz.net>
2013-11-08 15:55:54 +1100

    Create BeaconAdvertisementUtil.h
    Objective C Header file for generating iBeacon advertisement data

---
+++ BeaconAdvertisementUtil.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+@interface BeaconAdvertisementUtil : NSObject
+
+@property (strong,nonatomic) NSUUID *proximityUUID;
+@property (assign,nonatomic) uint16_t major;
+@property (assign,nonatomic) uint16_t minor;
+@property (assign,nonatomic) int8_t measuredPower;
+
+- (id)initWithProximityUUID:(NSUUID *)proximityUUID
+                      major:(uint16_t)major
+                      minor:(uint16_t)minor
+              measuredPower:(int8_t)power;
+
+
+- (NSDictionary *)beaconAdvertisement;
+
+@end
